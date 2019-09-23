8ad71c569f2fddb40933bc4f097d96bf42a8fa18
Nicolas Phister] <nicolas@phister.fr>
2019-09-23 17:12:21 +0200

    register TextureModelComponent for debug purposes

--- src/types/registerModelComponent.cpp
+++ src/types/registerModelComponent.cpp
@@ -1,8 +1,10 @@
 #include "registerTypes.hpp"
 #include "components/ModelComponent.hpp"
+#include "components/TextureModelComponent.hpp"
 
 void registerModelComponent(kengine::EntityManager & em) {
 	registerComponents<
-		kengine::ModelComponent
+		kengine::ModelComponent,
+		kengine::TextureModelComponent
 	>(em);
 }
