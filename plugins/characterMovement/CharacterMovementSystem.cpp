bc118b5731a1f036784e3ad8cee7d543d3428ff6
Nicolas Phister] <nicolas@phister.fr>
2019-11-29 09:41:06 +0100

    fix character rotation direction

--- plugins/characterMovement/CharacterMovementSystem.cpp
+++ plugins/characterMovement/CharacterMovementSystem.cpp
@@ -30,6 +30,8 @@ CharacterMovementSystem::CharacterMovementSystem(kengine::EntityManager & em) : 
 	em += [](kengine::Entity & e) { e += kengine::AdjustableComponent("[Character/Movement] Yaw offset", &YAW_OFFSET); };
 }
 
+static float getRotationDirection(float angleDelta);
+static void debug(kengine::EntityManager & em, kengine::Entity & e, const putils::Point3f & pos, const putils::Point3f & target);
 void CharacterMovementSystem::execute() {
 	for (auto & [e, transform, physics, movement] : _em.getEntities<kengine::TransformComponent3f, kengine::PhysicsComponent, CharacterMovementComponent>()) {
 		e += kengine::KinematicComponent{};
@@ -46,18 +48,30 @@ void CharacterMovementSystem::execute() {
 		const auto yawTo = pos.getYawTo(movement.destination);
 		const auto yawDelta = transform.yaw - putils::constrainAngle(yawTo + YAW_OFFSET);
 		if (std::abs(yawDelta) > FACING_STRICTNESS)
-			physics.yaw = TURN_SPEED * -putils::sign(yawDelta);
+			physics.yaw = TURN_SPEED * getRotationDirection(yawDelta);
 		else
 			physics.yaw = 0.f;
 
 		const auto pitchTo = pos.getPitchTo(movement.destination);
 		const auto pitchDelta = transform.pitch - pitchTo;
 		if (std::abs(pitchDelta) > FACING_STRICTNESS)
-			physics.pitch = TURN_SPEED * -putils::sign(pitchDelta);
+			physics.pitch = TURN_SPEED * getRotationDirection(pitchDelta);
 		else
 			physics.pitch = 0.f;
 
 #ifndef KENGINE_NDEBUG
+		debug(_em, e, pos, movement.destination);
+#endif
+	}
+}
+
+static float getRotationDirection(float angleDelta) {
+	return std::abs(angleDelta) > putils::pi ?
+		putils::sign(angleDelta) : -putils::sign(angleDelta);
+}
+
+#ifndef KENGINE_NDEBUG
+static void debug(kengine::EntityManager & em, kengine::Entity & e, const putils::Point3f & pos, const putils::Point3f & target) {
 		struct DebugEntityComponent {
 			kengine::Entity::ID id = kengine::Entity::INVALID_ID;
 		};
@@ -65,29 +79,17 @@ void CharacterMovementSystem::execute() {
 		// DEBUG
 		if (!e.has<DebugEntityComponent>()) {
 			auto & comp = e.attach<DebugEntityComponent>();
-			_em += [&](kengine::Entity & e) {
+			em += [&](kengine::Entity & e) {
 				comp.id = e.id;
 				e += kengine::DebugGraphicsComponent(kengine::DebugGraphicsComponent::Line);
 				e += kengine::TransformComponent3f{};
 			};
 			e += kengine::DebugGraphicsComponent(kengine::DebugGraphicsComponent::Sphere, { {}, { .1f, .1f, .1f } });
-
-			e += kengine::ImGuiComponent([=] {
-				if (ImGui::Begin("Movement")) {
-					ImGui::Text("Yaw to target: %f", yawTo);
-					ImGui::Text("Yaw delta: %f", yawDelta);
-
-					ImGui::Text("Pitch to target: %f", pitchTo);
-					ImGui::Text("Pitch delta: %f", pitchDelta);
-				}
-				ImGui::End();
-			});
 		}
 		// /DEBUG
-#endif
 
-		auto & debug = _em.getEntity(e.get<DebugEntityComponent>().id).attach<kengine::DebugGraphicsComponent>();
+		auto & debug = em.getEntity(e.get<DebugEntityComponent>().id).attach<kengine::DebugGraphicsComponent>();
 		debug.offset.position = pos;
-		debug.lineEnd = movement.destination;
-	}
+		debug.lineEnd = target;
 }
+#endif
