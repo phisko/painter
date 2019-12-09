e874d3c584b80096c4e104be521f2da853884254
Nicolas Phister] <nicolas@phister.fr>
2019-12-09 15:21:05 +0100

    cleanup

--- plugins/characterMovement/CharacterMovementSystem.cpp
+++ plugins/characterMovement/CharacterMovementSystem.cpp
@@ -37,8 +37,7 @@ EXPORT void loadKenginePlugin(kengine::EntityManager & em) {
 	};
 }
 
-static int getRotationDirection(float angleDelta);
-static void debug(kengine::EntityManager & em, kengine::Entity & e, const putils::Point3f & pos, const putils::Point3f & target);
+static void debug(kengine::Entity & e, const putils::Point3f & pos, const putils::Point3f & target);
 static void execute(float deltaTime) {
 	for (auto & [e, transform, physics, movement] : g_em->getEntities<kengine::TransformComponent3f, kengine::PhysicsComponent, CharacterMovementComponent>()) {
 		e += kengine::KinematicComponent{};
@@ -56,47 +55,37 @@ static void execute(float deltaTime) {
 
 		const auto direction = putils::normalized(movement.destination - pos);
 		const auto yawTo = putils::getYawFromNormalizedDirection(direction);
-		const auto yawDelta = transform.yaw - putils::constrainAngle(yawTo);
-		physics.yaw = TURN_SPEED * getRotationDirection(yawDelta);
+		const auto yawDelta = putils::constrainAngle(putils::constrainAngle(yawTo) - transform.yaw);
+		physics.yaw = TURN_SPEED * putils::sign(yawDelta);
 
 		const auto pitchTo = putils::getPitchFromNormalizedDirection(direction);
-		const auto pitchDelta = transform.pitch - pitchTo;
-		physics.pitch = TURN_SPEED * getRotationDirection(pitchDelta);
+		const auto pitchDelta = putils::constrainAngle(putils::constrainAngle(pitchTo) - transform.pitch);
+		physics.pitch = TURN_SPEED * putils::sign(pitchDelta);
 
 #ifndef KENGINE_NDEBUG
-		debug(*g_em, e, pos, movement.destination);
+		debug(e, pos, movement.destination);
 #endif
 	}
 }
 
-static int getRotationDirection(float angleDelta) {
-	const auto absDelta = std::abs(angleDelta);
-	if (absDelta <= FACING_STRICTNESS)
-		return 0;
-	else if (absDelta > putils::pi) // Turn the other way
-		return putils::sign(angleDelta);
-	else
-		return -putils::sign(angleDelta);
-}
-
 #ifndef KENGINE_NDEBUG
-static void debug(kengine::EntityManager & em, kengine::Entity & e, const putils::Point3f & pos, const putils::Point3f & target) {
-		struct DebugEntityComponent {
-			kengine::Entity::ID id = kengine::Entity::INVALID_ID;
+static void debug(kengine::Entity & e, const putils::Point3f & pos, const putils::Point3f & target) {
+	struct DebugEntityComponent {
+		kengine::Entity::ID id = kengine::Entity::INVALID_ID;
+	};
+
+	if (!e.has<DebugEntityComponent>()) {
+		auto & comp = e.attach<DebugEntityComponent>();
+		*g_em += [&](kengine::Entity & e) {
+			comp.id = e.id;
+			e += kengine::DebugGraphicsComponent(kengine::DebugGraphicsComponent::Line);
+			e += kengine::TransformComponent3f{};
 		};
+		e += kengine::DebugGraphicsComponent(kengine::DebugGraphicsComponent::Sphere, { {}, { .1f, .1f, .1f } });
+	}
 
-		if (!e.has<DebugEntityComponent>()) {
-			auto & comp = e.attach<DebugEntityComponent>();
-			em += [&](kengine::Entity & e) {
-				comp.id = e.id;
-				e += kengine::DebugGraphicsComponent(kengine::DebugGraphicsComponent::Line);
-				e += kengine::TransformComponent3f{};
-			};
-			e += kengine::DebugGraphicsComponent(kengine::DebugGraphicsComponent::Sphere, { {}, { .1f, .1f, .1f } });
-		}
-
-		auto & debug = em.getEntity(e.get<DebugEntityComponent>().id).attach<kengine::DebugGraphicsComponent>();
-		debug.offset.position = pos;
-		debug.lineEnd = target;
+	auto & debug = g_em->getEntity(e.get<DebugEntityComponent>().id).attach<kengine::DebugGraphicsComponent>();
+	debug.offset.position = pos;
+	debug.lineEnd = target;
 }
 #endif
