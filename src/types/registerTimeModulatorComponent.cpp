1163baa88dcd285dc06d89e99ff53ba8b3b54894
Nicolas Phister] <nicolas@phister.fr>
2020-05-11 15:05:58 +0200

    merge

--- src/types/registerTimeModulatorComponent.cpp
+++ src/types/registerTimeModulatorComponent.cpp
@@ -5,8 +5,4 @@ void registerTimeModulatorComponent(kengine::EntityManager & em) {
 	registerComponents<
 		kengine::TimeModulatorComponent
 	>(em);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b31e1c063e8d6fbbf0430492205f030a7bf75fc9
